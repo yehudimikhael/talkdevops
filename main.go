52248825caf05f68a409de79f77baf4f4834c049
Yehudi Brito <yehudimikhael@gmail.com>
2022-11-10 17:43:56 -0300

    Update main.go
    Adicionando comentários no código.

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"time"
-	"fmt"
+	"fmt" //FMT -> O Pacato FMT implementa  asf unções de I/O(Entrada e Saída) análogas ao Printf e o Scanf de C em GO
 	vegeta "github.com/tsenart/vegeta/v12/lib"
 )
 
 
 func main(){
 
-	rate := vegeta.Rate{Freq: 100, Per: time.Second}
-	duration := 5*time.Second
+	rate := vegeta.Rate{Freq: 100, Per: time.Second} //Fazer 100 request a cada segundo
+	duration := 5*time.Second // E vamos fazer isso por 5s
+	// Definir nosso alvo 
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
 		URL: "https://google.com.br",
@@ -19,12 +20,13 @@ func main(){
 	attacker := vegeta.NewAttacker()
 
 	var metrics vegeta.Metrics 
-
+	
+	// Vamos realizar as requisições
 	for res := range attacker.Attack(targeter, rate, duration, "Attack!"){
-		metrics.Add(res)
+		metrics.Add(res) //Atribuindo o resultado ao objeto
 	}
 
 	metrics.Close()
-
-	fmt.Println("Resultado: ", metrics.Latencies.P99)
+	
+	fmt.Println("Resultado: ", metrics.Latencies.P99) 
 }
